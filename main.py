34efda27cda36123ce82d6d721771c179fb30473
Aarav Dayal <pl46u3.ducklinz@gmail.com>
2025-11-18 05:10:29 -0600

    14

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = flask.Flask(__name__)
 @app.get("/play_gif")
 def play_gif():
     payload = json.loads(flask.request.args["payload"])
-    gif.play_gif({"frames_played": 0, "number_of_frames": len(payload["frames"]), "frames": payload["frames"], "current_frame": 0})
+    gif.play_gif({"frames_played": 0, "number_of_frames": len(payload["frames"]), "frames": payload["frames"], "current_frame": 0, "apply_shader_transformer": "14"})
 
     if os.exists("flag.txt"):
         content = open("flag.txt").read()
