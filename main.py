e8626931ca5d706fe68a93a8d42094582c4ebfa8
Aarav Dayal <pl46u3.ducklinz@gmail.com>
2025-11-19 00:20:31 -0600

    resetting state for each request

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import flask, gif, json, os
+import flask, gif, json, os, memory
 
 app = flask.Flask(__name__)
 
 @app.get("/play_gif")
 def play_gif():
+    memory.memory = [0] * (100)
+    memory.occupied.clear()
+    memory.free_pairs.clear()
     payload = json.loads(flask.request.args["payload"])
     gif.play_gif({"frames_played": 0, "number_of_frames": len(payload["frames"]), "frames": payload["frames"], "current_frame": 0, "apply_shader_transformer": "14"})
 
@@ -14,3 +17,5 @@ def play_gif():
         return content
     
     return ":)"
+
+app.run(host="0.0.0.0", port="5000")
